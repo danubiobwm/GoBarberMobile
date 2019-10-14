8d219663daa7e41e232d302e1566f82aa4507813
danubiobwm <danubio.bwm@gmail.com>
2019-10-14 09:47:57 -0300

    Connecting Redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -3,7 +3,7 @@ import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: 'danubio' })
+  const tron = Reactotron.configure({ host: '192.168.1.15' })
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
